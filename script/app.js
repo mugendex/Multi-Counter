d985b5e9c273aaa5c3ce6a5f17906be9baa10140
mugendex <tanariku6134@gmail.com>
2018-07-09 17:21:28 +0900

    数字が変だったのde修正

--- script/app.js
+++ script/app.js
@@ -33,39 +33,44 @@ var NCNum = 0;//カウンターの数のカウント
 // コインカウンターを追加するメソッド
 function CreateNC(parentId){
     //まだ存在しない場合
-    //if(document.getElementById(`${parentId}_NC`) == null){
+    //if(document.getElementById(`${parentId}_NC_${NCNum}`) == null){
         var parent = document.getElementById(parentId);
+
+        NCNum++;
 
         // 本体
         var NCWrapper = document.createElement("div");
-        NCWrapper.classList.add(`NCWrapper`,`${parentId}`);
-        NCWrapper.id = `${parentId}_NC$`;
+        NCWrapper.classList.add("NCWrapper",`${parentId}`);
+        NCWrapper.id = `${parentId}_NC_${NCNum}`;
         parent.appendChild(NCWrapper);
         
         // 値表示部
         var counter = document.createElement("div");
-        counter.classList.add(`${parentId}_NC`);
-        counter.id = `${parentId}NC_out`;
+        counter.classList.add("NCContent",`${parentId}_NC_${NCNum}`);
+        counter.id = `${parentId}NC_out${NCNum}`;
         counter.textContent = "0";
                 
         
         // +ボタン
         var plusButton = document.createElement("button");
             plusButton.textContent = "＋";
+            plusButton.classList.add("NCContent","btn",`${parentId}`);
             plusButton.onclick = function(){
-            increment(`${parentId}NC_out`);
+            increment(counter.id);
         };
     
         // -ボタン
         var minusButton = document.createElement("button");
         minusButton.textContent = "－";
+        minusButton.classList.add("NCContent","btn",`${parentId}`);
         minusButton.onclick = function(){
-            decrement(`${parentId}NC_out`);
+            decrement(counter.id);
         };
 
         // 削除ボタン
         var removeButton = document.createElement("Button");
         removeButton.textContent = "x";
+        removeButton.classList.add("NCContent","btn",`${parentId}`);
         removeButton.onclick = function(){
             minusButton.remove();
             plusButton.remove();
@@ -92,19 +97,22 @@ function Createio(parentId) {
     var ioWrapper;
     if(document.getElementById(`${parentId}ioWrapper`) == null){
         ioWrapper = document.createElement("div");
-        ioWrapper.classList.add("ioWrapper");
+        ioWrapper.classList.add("ioWrapper",`${parentId}`);
         ioWrapper.id = `${parentId}ioWrapper`;
 
         parent.appendChild(ioWrapper);
-
+        
+        //削除ボタン
         ioremove = document.createElement("button");
         ioremove.textContent = "X";
-        ioremove.classList.add("ioContent",`${parentId}`);
+        ioremove.classList.add("ioContent","btn",`${parentId}`);
         ioremove.onclick = function(){
-            if(ioWrapper.childNodes[1]){
+            if(ioWrapper.childNodes[2]){ //削除ボタン以外の小要素が2つ以上の場合
                 ioWrapper.childNodes[1].remove();
             }else{
-                ioWrapper.childNodes[0].remove();
+                //ioカウンター削除
+                ioWrapper.childNodes[1].remove();
+                this.remove();
                 ioWrapper.remove();
             }
         }
@@ -119,7 +127,7 @@ function Createio(parentId) {
     //ボタン
     var io = document.createElement("input");
     io.type = "checkbox";
-    io.classList.add("ioContent",`${parentId}`);
+    io.classList.add("ioContent","cBox",`${parentId}`);
 
     //小要素を親要素に追加
     ioWrapper.appendChild(io);
